5fbdf8596bb0c79bcd158c9ed1de520d3ed17b15
Aaron Meurer <asmeurer@gmail.com>
2013-07-02 11:29:14 -0500

    Add a simple test of the bug I am seeing

---
+++ test3.py
@@ -0,0 +1,16 @@
+import sys
+
+from numba import autojit
+
+autojit = autojit if int(sys.argv[1]) else lambda x: x
+
+@autojit
+def f(x):
+    acc = 0
+    i = 0
+    for elem in x:
+        acc += elem*i
+        i += 1
+    return acc
+
+print f(range(10))
