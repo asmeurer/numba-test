cafc67b20d64db34d8179c7051eb0cd42b8619a0
Aaron Meurer <asmeurer@gmail.com>
2013-07-11 15:20:33 -0500

    Add tests and timings for fabs

--- test4.py
+++ test4.py
@@ -1,7 +1,21 @@
 from numba import jit
 
+@jit('void(float64[:])')
+def g(X):
+    for i in X:
+        pass
+
+
+# @jit('void()')
+# def rangeloop():
+#     for i in h(10):
+#         pass
+#
+# def h():
+#     return [1, 2, 3]
+
 @jit('void()')
 def f():
-    x = range(100)
+    x = range(10)
     for i in x:
         pass
