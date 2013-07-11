b229add21c65d89ec02a0ae84401f3c723171e59
Aaron Meurer <asmeurer@gmail.com>
2013-07-11 15:13:48 -0500

    Use algorithms that work with numba

--- math-test.py
+++ math-test.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 from __future__ import division
 
+import sys
 import time
 import math
 from numba import autojit
@@ -8,12 +9,12 @@ from numba import autojit
 def sqrt_test(N):
     ans = 0
     for i in range(N):
-        ans = math.sqrt(i + ans)
+        ans = math.sqrt(i) + ans
 
 def log_test(N):
     ans = 1
-    for i in range(N):
-        ans = math.log(i + ans)
+    for i in range(1, N):
+        ans = math.log(i) + ans
 
 def exp_test(N):
     ans = 0
@@ -21,11 +22,14 @@ def exp_test(N):
         ans = math.exp(i/100) + ans
 
 def main():
-    N = 10000
+    if len(sys.argv) > 1:
+        N = int(sys.argv[1])
+    else:
+        N = 10000
 
     for name in [
-        #'sqrt',
-        #'log',
+        'sqrt',
+        'log',
         'exp',
     ]:
         print '%s python: ' % name,
